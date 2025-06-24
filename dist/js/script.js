5706fdc67df22d21e35e9fa9016d363b1a1caa5f
ralitsavoronevska <ralicavoronevska@gmail.com>
2025-06-25 00:02:49 +0300

    Optimizing the Project's Code

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,23 +8,19 @@ let productsSeniorLevel = document.querySelector("#pricing .senior-level");
 let root = document.querySelector(':root');
 let header = document.querySelector("#header");
 let fontawesomeSvgs = document.querySelectorAll(".fa-primary");
+const outline = ["btn-outline", "primary-outline"];
+const text = ["gradient-text", "primary-text"];
 
 function switchUnchecked() {
   root.style.setProperty('--primary', 'var(--green)');
   root.style.setProperty('--secondary', 'var(--blue)');
   header.style.boxShadow = "3px 3px 12px rgba(66, 184, 131, .25)";
-  seniorLevel.classList.remove("btn-outline");
-  seniorLevel.classList.remove("primary-outline");
-  midLevel.classList.add("btn-outline");
-  midLevel.classList.add("primary-outline");
-  seniorLevelHeading.classList.remove("gradient-text", "primary-text");
-  midLevelHeading.classList.remove("grayed-out-text");
-  midLevelHeading.classList.add("gradient-text", "primary-text");
-  productsSeniorLevel.classList.remove("d-flex");
-  productsSeniorLevel.classList.add("d-none");
-  productsMidLevel.classList.remove("d-none");
-  productsMidLevel.classList.add("d-flex");
-  seniorLevelHeading.classList.add("grayed-out-text");
+  seniorLevel.classList.remove(...outline);
+  midLevel.classList.add(...outline);
+  seniorLevelHeading.classList.remove(...text).add("grayed-out-text");
+  midLevelHeading.classList.remove("grayed-out-text").add(...text);
+  productsSeniorLevel.classList.remove("d-flex").add("d-none");
+  productsMidLevel.classList.remove("d-none").add("d-flex");
   fontawesomeSvgs.forEach((svg) => { svg.classList.remove("pink-filter"); })
   fontawesomeSvgs.forEach((svg) => { svg.classList.add("green-filter");})
 }
@@ -33,18 +29,12 @@ function switchChecked() {
   root.style.setProperty('--primary', 'var(--pink)');
   root.style.setProperty('--secondary', 'var(--purple)');
   header.style.boxShadow = "3px 3px 12px rgba(248, 100, 189, .25)";
-  midLevel.classList.remove("btn-outline");
-  midLevel.classList.remove("primary-outline");
-  seniorLevel.classList.add("btn-outline");
-  seniorLevel.classList.add("primary-outline");
-  midLevelHeading.classList.remove("gradient-text", "primary-text");
-  seniorLevelHeading.classList.add("gradient-text", "primary-text");
-  productsMidLevel.classList.remove("d-flex");
-  productsMidLevel.classList.add("d-none");
-  productsSeniorLevel.classList.remove("d-none");
-  productsSeniorLevel.classList.add("d-flex");
-  seniorLevelHeading.classList.remove("grayed-out-text");
-  midLevelHeading.classList.add("grayed-out-text");
+  midLevel.classList.remove(...outline);
+  seniorLevel.classList.add(...outline);
+  midLevelHeading.classList.remove(...text).add("grayed-out-text");
+  productsMidLevel.classList.remove("d-flex").add("d-none");
+  productsSeniorLevel.classList.remove("d-none").add("d-flex");
+  seniorLevelHeading.classList.remove("grayed-out-text").add(...text);
   fontawesomeSvgs.forEach((svg) => { svg.classList.remove("green-filter"); })
   fontawesomeSvgs.forEach((svg) => { svg.classList.add("pink-filter"); })
 }
