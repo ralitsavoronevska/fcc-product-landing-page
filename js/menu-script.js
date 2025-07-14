8f8d19b0ac92e32e30863df5aaf55eccd5c34527
ralitsavoronevska <ralicavoronevska@gmail.com>
2025-07-14 18:51:15 +0300

    Updates

--- js/menu-script.js
+++ js/menu-script.js
@@ -1,11 +1,15 @@
 window.addEventListener("DOMContentLoaded", () => {
 	const burger = document.querySelector(".header-burger");
 	const navWrapper = document.querySelector(".header-navigation-wrapper");
+	const logo = document.querySelector("#header-img");
+	const darkLogo = document.querySelector("#header-img-dark");
 
 	if (burger && navWrapper) {
 		burger.addEventListener("click", () => {
 			burger.classList.toggle("active");
 			navWrapper.classList.toggle("open");
+			darkLogo.classList.toggle("d-none");
+			logo.classList.toggle("d-none");
 		});
 	}
 
